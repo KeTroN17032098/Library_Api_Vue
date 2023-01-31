7063c9551060ef721ee1757b35e0b9d52c64fa4f
KeTroN17032098 <ketron12@kakao.com>
2023-01-31 23:59:30 +0900

    change
    secret json

--- Library/Library/settings.py
+++ Library/Library/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import datetime
 import os
 from django.utils.translation import gettext_lazy as _
-
+import json
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,8 +25,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jt^2n!l!2lmtff#$4#yzu9w__9z+17(0m*v-jb2k*4smfjq)0u'
+secret_file = os.path.join(BASE_DIR, 'secret/secret.json')  # secrets.json 파일 위치를 명시
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+def get_secret(setting):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
 
+
+SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
