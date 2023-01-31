5822517599f4d253fff4a57e6d2559a9c6729ef4
KeTroN17032098 <ketron12@kakao.com>
2023-02-01 02:01:17 +0900

    adding vue

--- Library/Library/settings.py
+++ Library/Library/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'corsheaders',
     #My Apps
     'users', 
     'blacklist',
@@ -73,6 +74,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
@@ -195,3 +197,9 @@ LOGIN_REDIRECT_URL =''
 # 미디어 파일의 경로 설정
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 MEDIA_URL = "/media/"
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8080',  
+    'http://127.0.0.1:8080'
+)
---
+++ lws/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <todo-header></todo-header>
+    <v-parallax src="http://localhost:8000/static/img/bg.webp">
+      <todo-content></todo-content>
+    </v-parallax>
+    <todo-footer></todo-footer>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue';
+import TodoContent from './components/TodoContent.vue';
+import TodoFooter from './components/TodoFooter.vue';
+
+export default {
+  data: function() {
+    return {
+      name: 'cslee'
+    }    
+  },
+components: {
+  'todo-header': TodoHeader,
+  'todo-content': TodoContent,
+  'todo-footer': TodoFooter,
+}
+}
+</script>
+
+<style>
+
+</style>
